127ccbd417e9af331c9a02bb4ea8d8cb24cf6256
DESKTOP-4MRR27M\razal <razalexraz@gmail.com>
2017-11-09 18:09:06 +0300

    Пересоздал

--- Equation/Equation/Form1.cs
+++ Equation/Equation/Form1.cs
@@ -24,7 +24,15 @@ namespace Equation
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            
+        }
+        private double[] Calculate(double a,double b,double c)
+        {
+            double[] result = new double[2];
+            double d = Math.Pow(b, 2.0) - 4.0 * a * c;
+            result[0] = (-b + Math.Sqrt(d)) / (2.0 * a);
+            result[1] = (-b - Math.Sqrt(d)) / (2.0 * a);
+            return result;
         }
     }
 }
