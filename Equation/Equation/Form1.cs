dedfc64b301aaa12530be3da9576a3c8070725db
Dmitry Patrin <patrin48rus@ya.ru>
2017-11-09 18:19:50 +0300

    Equation works but not fine

--- Equation/Equation/Form1.cs
+++ Equation/Equation/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace Equation
 {
     public partial class Form1 : Form
@@ -24,14 +26,19 @@ namespace Equation
 
         private void button1_Click(object sender, EventArgs e)
         {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            double c = Convert.ToDouble(textBox3.Text);
+            double[] result = Calculate(a, b, c);
+            label4.Text = "X1= " + result[0].ToString() + " X2= " + result[1].ToString();
             
         }
         private double[] Calculate(double a,double b,double c)
         {
             double[] result = new double[2];
             double d = Math.Pow(b, 2.0) - 4.0 * a * c;
-            result[0] = (-b + Math.Sqrt(d)) / (2.0 * a);
-            result[1] = (-b - Math.Sqrt(d)) / (2.0 * a);
+            result[0] = -1.0*b + Math.Sqrt(d) / (2.0 * a);
+            result[1] = (-1.0*b - Math.Sqrt(d)) / (2.0 * a);
             return result;
         }
     }
